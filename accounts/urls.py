35fb921f5d3d02e77a0c76a4cd69a00f8eb7372c
Nathan Magyar <magyarn@umich.edu>
2019-02-16 09:28:45 -0500

    Add signup view and navbar display logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,6 @@ app_name = 'accounts'
 
 urlpatterns = [
   url(r'login/$', views.LoginView.as_view(), name="login"),
+  url(r'logout/$', views.LogoutView.as_view(), name="logout"),
+  url(r'signup/$', views.SignupView.as_view(), name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse_lazy
 from django.views import generic
+
+from . import forms
 
 class LoginView(generic.FormView):
   form_class = AuthenticationForm
@@ -18,5 +20,17 @@ class LoginView(generic.FormView):
   def form_valid(self, form):
     login(self.request, form.get_user())
     return super().form_valid(form)
+
+class LogoutView(generic.RedirectView):
+  url = reverse_lazy("courses:home")
+
+  def get(self, *args, **kwargs):
+    logout(self.request)
+    return super().get(self.request, *args, **kwargs)
+
+class SignupView(generic.CreateView):
+  form_class = forms.CustomUserCreationForm
+  success_url = reverse_lazy("accounts:login")
+  template_name = "accounts/signup.html"
 
 
